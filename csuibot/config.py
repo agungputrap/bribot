5269c26ee9ffa2062c65f6e6d3807f9769415745
Agung Putra Pasaribu <real.stereo2@gmail.com>
2016-09-30 01:02:41 +0700

    change config.py into new bot

--- csuibot/config.py
+++ csuibot/config.py
@@ -3,6 +3,6 @@ from os import environ
 
 APP_ENV = environ.get('APP_ENV', 'development')
 DEBUG = environ.get('DEBUG', 'true') == 'true'
-TELEGRAM_BOT_TOKEN = environ.get('TELEGRAM_BOT_TOKEN', '273949527:AAGAWQ_Z4Zh9qdg5w2nh_UBZXbXvE52DbBs')
+TELEGRAM_BOT_TOKEN = environ.get('TELEGRAM_BOT_TOKEN', '276003281:AAEecGdM7wZO-97qFL6mzSXWBkzyQRoQ0gQ')
 LOG_LEVEL = environ.get('LOG_LEVEL', 'DEBUG')
 WEBHOOK_HOST = environ.get('WEBHOOK_HOST', 'https://agungbot.herokuapp.com')
